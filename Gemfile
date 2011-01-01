c920c296febb8469d13f58e23dc4261360cdb537
Julian Kornberger <jk+github@digineo.de>
2011-01-01 18:44:02 +0100

    Bundler eingebaut

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source :gemcutter
+gem "rails", "~> 2.3.10"
+gem "mysql"
+
+gem "mechanize"
+gem "paperclip"
+
+gem 'haml'
+gem 'inherited_resources', '~> 1.0.6'
+gem 'formtastic', '~> 1.2'
+
+
+group :development do
+  # bundler requires these gems in development
+  # gem "rails-footnotes"
+end
+
+
+group :test do
+  # bundler requires these gems while running tests
+  # gem "rspec"
+  # gem "faker"
+end
--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,5 @@
 # Be sure to restart your server when you modify this file
 
-# Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.10' unless defined? RAILS_GEM_VERSION
-
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
@@ -13,14 +10,6 @@ Rails::Initializer.run do |config|
 
   # Add additional load paths for your own custom dirs
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
-
-  # Specify gems that this application depends on and have them installed with rake gems:install
-  config.gem "mechanize"
-  config.gem "paperclip"
-  
-  config.gem 'haml'
-  config.gem 'inherited_resources', :version => '=1.0.6'
-  config.gem 'formtastic'
   
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
