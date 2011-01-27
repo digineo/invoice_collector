dbb7c49e658033a1831bd9aa5d7cf28b027db72b
Julian Kornberger <jk+github@digineo.de>
2011-01-27 11:30:01 +0100

    Regexp für Rechnungsnummern erweitert

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -6,7 +6,7 @@ class Invoice < ActiveRecord::Base
   has_attached_file :signature, :path => ':rails_root/data/invoices/:id/signature.:extension'
   
   validates_presence_of :account_id
-  validates_format_of   :number, :with => /^[A-Z0-9_-]+$/i
+  validates_format_of   :number, :with => /^[A-Z0-9._-]+$/i
   validates_attachment_presence :original
   
   scope :latest, :order => 'date DESC', :limit => 10
